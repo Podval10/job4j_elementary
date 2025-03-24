220ef9d47f45d18cca2da4fddc7125d078f25748
andre <kitksiomi@gmail.com>
2025-03-24 16:56:35 +0300

    4. Тесты для стороны прямоугольника.

--- src/main/java/ru/job4j/calculator/RectangleArea.java
+++ src/main/java/ru/job4j/calculator/RectangleArea.java
@@ -1,7 +1,7 @@
 package ru.job4j.calculator;
 
 public class RectangleArea {
-    public static double sguare(double p,double k){
+    public static double square(double p,double k){
 double deistvie1=k+1;
 double deistvie2= deistvie1*2;
 double vusota= p/deistvie2;
@@ -11,20 +11,20 @@ double result=dlina*vusota;
 return result;
     }
 
-    public static double sguareNew(double p,double k) {
+    public static double squareNew(double p,double k) {
         return p / (2 * (k + 1)) * k * (p / ((k + 1)*2));
 
     }
 
 
 public static void main(String[] args){
-        double result=RectangleArea.sguare(4,1);
-        double result2=RectangleArea.sguare(6,2);
+        double result=RectangleArea.square(4,1);
+        double result2=RectangleArea.squareNew(6,2);
 
         System.out.println("p=4,k=1,s=1,real="+result);
         System.out.println("p=6,k=2, "+ result2);
 
-        double result3=RectangleArea.sguareNew(6,2);
-        System.out.println("p=4,k=1,s=1,real="+result3);
+        double result3=RectangleArea.squareNew(6,3);
+        System.out.println("p=6,k=3,s=1,real="+result3);
 }
 }
