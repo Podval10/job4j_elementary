e2355ba8e71a12f88eeb5f0711bcbaca1ffc9cb8
andre <kitksiomi@gmail.com>
2025-04-02 15:35:20 +0300

    5.0. Цикл for. Определение.

---
+++ src/main/java/ru/job4j/condition/LoopFor.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class LoopFor {
+
+        public static void main(String[] args) {
+            for (int index = 0; index < 5; index++) {
+                System.out.println("Первый случай "  + index);
+            }
+                    for (int index1 = 5; index1 <= 10; index1++) {
+                        System.out.println(index1);
+                    }
+
+            }
+        }
+
