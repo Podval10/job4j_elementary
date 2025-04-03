bcc8a4d79c1b7a109fb90978547855063d2285d8
andre <kitksiomi@gmail.com>
2025-04-03 13:06:53 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне

--- src/test/java/ru/job4j/condition/CounterTest.java
+++ src/test/java/ru/job4j/condition/CounterTest.java
@@ -25,4 +25,48 @@ class CounterTest {
         int expected=33;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void SumPlusIndexResult1() {
+        int start=1;
+        int finish=1;
+        int result= Counter.sum(start,finish);
+        int expected=1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void SumByEvenResult30() {
+        int start=0;
+        int finish=10;
+        int result= Counter.sumByEven(start,finish);
+        int expected=30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void SumByEvenResult18 () {
+        int start=3;
+        int finish=8;
+        int result= Counter.sumByEven(start,finish);
+        int expected=18;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void SumByEvenResult0 () {
+        int start=1;
+        int finish=1;
+        int result= Counter.sumByEven(start,finish);
+        int expected=0;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    void SumByEvenResult24 () {
+        int start=-4;
+        int finish=10;
+        int result= Counter.sumByEven(start,finish);
+        int expected=24;
+        assertThat(result).isEqualTo(expected);
+    }
 }
